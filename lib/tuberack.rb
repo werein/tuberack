f877a152d2fe86f8ca9eb08847e8a88bfab71ebc
Jiri Kolarik <jiri.kolarik@imin.cz>
2014-04-01 13:25:54 +0200

    Only on test environment

--- lib/tuberack.rb
+++ lib/tuberack.rb
@@ -1,6 +1,8 @@
-require 'tuberack/requirements'
-require 'tuberack/helpers'
-require 'tuberack/test_help'
+if ENV['RAILS_ENV'] == 'test'
+  require 'tuberack/requirements'
+  require 'tuberack/helpers'
+  require 'tuberack/test_help'
+end
 
 module Tuberack
 end
